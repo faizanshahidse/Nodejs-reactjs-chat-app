7841d2b864c5f9863a44a9fda6f9918620c6d2ea
divsync-faizans <faizans@divsync.com>
2022-03-22 19:05:00 +0500

    random

--- server/config/messages.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  default: {
-    success: 'Success',
-    fail: 'Fail',
-  },
-};
--- server/routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getAllUsers } = require('../controller/user.controller');
-
-router.get('/getAll', getAllUsers);
-
-module.exports = router;
